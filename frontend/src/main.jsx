db16b26807b1b92d1e41651c03dd1d96de7c90ba
jafar <jaffarhussssain@gmail.com>
2025-10-04 02:40:42 +0500

    add home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import App from './App.jsx'
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import ShopContextProvider from "./context/ShopeContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
+);
